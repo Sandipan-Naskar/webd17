9182d8e50ba27f86a95d9b899a622914af6d8f16
Sandipan-Naskar <sandipan.naskar.cse25@heritageit.edu.in>
2024-07-03 22:18:22 +0530

    files removed

--- javascript/minMaxFinder.js
+++
@@ -1,7 +0,0 @@
-function findMinMax() {
-    const array = document.getElementById('array').value.split(',').map(Number);
-    const min = Math.min(...array);
-    const max = Math.max(...array);
-
-    document.getElementById('result').textContent = `Min value: ${min}, Max value: ${max}`;
-}
